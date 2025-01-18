536a35bef6004570ec231386c684227a8424af83
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-01-17 23:10:24 -0500

    Building structure to get random prompt from list.

--- week02/Journal/prompts.cs
+++ week02/Journal/prompts.cs
@@ -2,5 +2,15 @@ using System;
 
 public class Prompts
 {
+    public List<string> _prompts;
+    string promptsFile = "prompts.txt";
 
+    public string GetPrompt()
+    {
+        
+        Random random = new Random();
+        int randomIndex = random.Next(_prompts.Count);
+
+        return _prompts[randomIndex];
+    }
 }
