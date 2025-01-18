ba90d529dae6b6272c267b848aa96ffa90e9f6a8
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-01-17 22:30:00 -0500

    Set up user menu

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write Journal Entry");
+            Console.WriteLine("2. Display Journal Entries");
+            Console.WriteLine("3. Load Journal");
+            Console.WriteLine("4. Save Journal");
+            Console.WriteLine("5. Quit");
+
+            string selection = Console.ReadLine();
+
+            switch (selection)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+                   
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    running = false;
+                    break;
+
+                default:
+                
+                    break;
+            }
+        }
     }
 }
