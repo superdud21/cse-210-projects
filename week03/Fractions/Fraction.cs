69f22cc917caba22dc73ef82b8cfddec2a50fb77
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-01-29 20:30:32 -0500

    Added three constructors

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,11 +1,25 @@
 using System;
+using System.Diagnostics.Contracts;
 
 public class Fraction
 {
     private int _top;
     private int _bottom;
 
-    
+    public Fraction()
+    {
+        
+    }
+
+    public Fraction(int topNumber)
+    {
+
+    }
+
+    public Fraction(int topNumber, int bottomNumber)
+    {
+
+    }
 
 
 }
