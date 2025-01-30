92e0c55a545d7e890e96bcf3349fb809450496e6
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-01-29 20:36:05 -0500

    Created 3 instances of Fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(5, 7);
     }
 }
