8a9b114ea575ca4e2e6a553f0e4b622b803da7bb
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 18:09:11 -0500

    Pulling in a random scripture, breaking it apart at the "|" and assinging it to variables.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,6 +8,13 @@ class Program
         {
             string[] scriptures = File.ReadAllLines("scriptures.txt");
 
+            Random random = new Random();
+            int randomScriptureIndex = random.Next(scriptures.Length);
+            string randomScripture = scriptures[randomScriptureIndex];
+            string [] sections = randomScripture.Split(" | ");
+            string scriptureReference = sections[0];
+            string scriptureText = sections[1];
+
             Console.WriteLine("Press Enter to hide words, or type 'quit' to exit.");
 
             while (true)
