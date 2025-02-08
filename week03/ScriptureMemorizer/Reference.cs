d5714a8419aeaa24de4a248027f806b7da8a12a7
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 17:00:24 -0500

    Setting up first Reference Constructor

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -1,12 +1,23 @@
 public class Reference
 {
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
     public Reference()
     {
 
     }
-
     public string GetDisplayText()
     {
-        
+
     }
 }
