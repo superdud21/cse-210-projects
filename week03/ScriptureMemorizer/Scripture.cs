e9cae1319d633d17914902bd7d692cf416b22f7a
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 16:53:24 -0500

    Setting up Scripture Class

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,22 @@
 public class Scripture
 {
+    public Scripture()
+    {
+
+    }
     
+    public void HideWords()
+    {
+
+    }
+
+    public bool IsCompletelyHidden() 
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        
+    }
 }
