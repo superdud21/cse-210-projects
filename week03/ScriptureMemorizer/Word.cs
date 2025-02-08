b625e744c8d32a6f6affceb949145bc5a4cd2f4d
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 16:56:01 -0500

    Setting up Word Class

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -1,4 +1,27 @@
-public class World
-{
-    
+public class Word
+{   
+    public Word()
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        
+    }
 }
