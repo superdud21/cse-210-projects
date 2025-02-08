61cade93b3fe1930d6b16cbd2d2b642ec85220d2
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 17:56:52 -0500

    Setting up quit functionality.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,9 +8,15 @@ class Program
         {
             string[] scriptures = File.ReadAllLines("scriptures.txt");
 
+            Console.WriteLine("Press Enter to hide words, or type 'quit' to exit.");
+
             while (true)
             {
-
+                string userInput = Console.ReadLine();
+                if (userInput == "quit")
+                {
+                    break;
+                }
             }
         } else {
             Console.WriteLine("No Scripture File Exists");
