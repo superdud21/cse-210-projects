19afe24a4311177d0c930dd263111df8bd4edb47
Caleb Cameron <36425378+superdud21@users.noreply.github.com>
2025-02-08 16:46:23 -0500

    Accessing Scriptures from file with "|" deliniation.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        List<Scripture> _scriptures = new List<Scripture>();
+
+        if (File.Exists("scriptures.txt"))
+        {
+            string[] scriptures = File.ReadAllLines("scriptures.txt");
+
+            foreach (string entry in scriptures)
+            {
+                Console.WriteLine(entry);
+            }
+        } else {
+            Console.WriteLine("No Scripture File Exists");
+        }
     }
 }
