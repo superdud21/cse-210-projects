a56744b192c3db3f8124be6467a22f291818f605
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 18:57:52 -0500

    Finished the Comment Class Constructor and Method

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -1,4 +1,16 @@
 class Comment
 {
+    private string _name;
+    private string _commentText;
 
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _commentText = text;
+    }
+
+    public string GetCommentInfo()
+    {
+        return $"- {_name}: {_commentText}";
+    }
 }
