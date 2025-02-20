ccb7d8856730b97a83ec5902d9cbcb8c30aa3939
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 23:31:11 -0500

    Activity class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,4 +1,39 @@
 class Activity
 {
-
+    private string _name;
+    private string _description;
+    private int _duration;
+    
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+    
+    public void DisplayIntro()
+    {
+        Console.WriteLine($"\n{_name} Activity");
+        Console.WriteLine(_description);
+        Console.Write("Enter duration in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowLoading(3);
+    }
+    
+    public void DisplayOutro()
+    {
+        Console.WriteLine("\nGreat job!");
+        Console.WriteLine($"You have completed the {_name} activity for {_duration} seconds.");
+        ShowLoading(3);
+    }
+    
+    public void ShowLoading(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("\n");
+    }
 }
