e5f797af1eed4a52954d44be3a0d2015fe33b071
Caleb Cameron <calebcam21@gmail.com>
2025-02-20 10:12:09 -0500

    Eternal Goal Class

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,4 +1,19 @@
-class EternalGoal
+class EternalGoal : Goal
 {
-    
+    public EternalGoal(string name, string description, int points) 
+        : base(name, description, points) { }
+
+    public override void RecordEvent()
+    {
+        Console.WriteLine($"Recorded progress for '{_shortName}'. Earned {_points} points.");
+    }
+
+    public override bool IsComplete() => false;
+
+    public override string GetDetailsString() => "[∞] " + _shortName;
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal|{_shortName}|{_description}|{_points}";
+    }
 }
