e4f1e4cc07d4c232e53a551d98a9d950fce062e4
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 20:07:52 -0500

    Address Class

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -1,4 +1,25 @@
 class Address
 {
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
 
+    public Address(string streetAddress, string city, string state, string country)
+    {
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _country = country;
+    }
+
+    public bool LivesInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetAddress()
+    {
+        return $"{_streetAddress}\n{_city}, {_state}\n{_country}";
+    }
 }
