b7e2f0efe6880bcbf3339c57ed93ad9eeb055701
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 19:29:17 -0500

    Completed Video Class with methods.

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -11,4 +11,32 @@ class Video
         _comments = new List<Comment>();
     }
 
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+
+    public string DisplayAllComments()
+    {
+        string output = "Comments:\n";
+        foreach (var comment in _comments)
+        {
+            output += comment.GetCommentInfo() + "\n";
+        }
+        return output;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {_name}");
+        Console.WriteLine($"Duration: {_videoDuration} seconds");
+        Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
+        Console.WriteLine(DisplayAllComments());
+        Console.WriteLine("\n----------------------\n");
+    }
 }
