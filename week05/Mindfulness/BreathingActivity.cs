8327936be8f91dec499820b3aa02b245be733fca
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 23:37:09 -0500

    Breathing Activity Class

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,4 +1,17 @@
-class BreathingActivity
+class BreathingActivity : Activity
 {
-
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through slow breathing. Clear your mind and focus on your breath.") {}
+    
+    public void Run()
+    {
+        DisplayIntro();
+        for (int i = 0; i < _duration / 6; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowLoading(3);
+            Console.WriteLine("Breathe out...");
+            ShowLoading(3);
+        }
+        DisplayOutro();
+    }
 }
