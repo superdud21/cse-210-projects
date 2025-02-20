d3d6312c3ea3800dc934d184a9fa83f91e4ed3b3
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 19:07:01 -0500

    Video Variables and constructor

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -1,4 +1,14 @@
 class Video
 {
-    
+    private string _name;
+    private double _videoDuration;
+    private List<Comment> _comments;
+
+    public Video(string name, double duration)
+    {
+        _name = name;
+        _videoDuration = duration;
+        _comments = new List<Comment>();
+    }
+
 }
