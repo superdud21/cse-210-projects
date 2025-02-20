5b9d2c9661ec7caae690d48215b2029bd999013a
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 19:52:06 -0500

    Product Class complete

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -1,4 +1,25 @@
 class Product
 {
-    
+    private string _name;
+    private string _productID;
+    private double _productPrice;
+    private int _productQuantity;
+
+    public Product(string name, string productID, double productPrice, int productQuantity)
+    {
+        _name = name;
+        _productID = productID;
+        _productPrice = productPrice;
+        _productQuantity = productQuantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return _productPrice * _productQuantity;
+    }
+
+    public string GetPackingLabel()
+    {
+        return $"{_name} (ID: {_productID})";
+    }
 }
