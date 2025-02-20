9fa5e19b4a5fd1875d3220d53878f1047d7d3ae1
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 23:46:50 -0500

    Listening Activity

--- week05/Mindfulness/ListeningActivity.cs
+++ week05/Mindfulness/ListeningActivity.cs
@@ -1,4 +1,39 @@
 class ListingActivity
 {
+    private static readonly List<string> Prompts = new()
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
     
+    private static readonly List<string> Questions = new()
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+    
+    public ReflectionActivity() : base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience.") {}
+    
+    public void Run()
+    {
+        DisplayIntro();
+        Console.WriteLine("\n" + Prompts[new Random().Next(Prompts.Count)]);
+        ShowLoading(3);
+        var startTime = DateTime.Now;
+        while ((DateTime.Now - startTime).TotalSeconds < _duration)
+        {
+            Console.WriteLine(Questions[new Random().Next(Questions.Count)]);
+            ShowLoading(5);
+        }
+        DisplayOutro();
+    }
 }
