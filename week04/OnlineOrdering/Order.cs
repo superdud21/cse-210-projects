f697fe86eb86c82280a8fb2c9c7d8b5e08908929
Caleb Cameron <calebcam21@gmail.com>
2025-02-19 20:12:26 -0500

    Order Class

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -1,4 +1,42 @@
 class Order
 {
+    private List<Product> _products;
+    private Customer _customer;
 
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+        foreach (var product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+        total += _customer.LivesInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in _products)
+        {
+            label += product.GetPackingLabel() + "\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return "Shipping Label:\n" + _customer.GetShippingLabel();
+    }
 }
