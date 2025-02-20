2e1dbaa119125afe97e267dc92496ee5e6fe10a7
Caleb Cameron <calebcam21@gmail.com>
2025-02-20 09:37:23 -0500

    Simple Goal Class.

--- week06/EternalQuest/SimpleGoal.cs
+++ week06/EternalQuest/SimpleGoal.cs
@@ -1,4 +1,28 @@
-class SimpleGoal
+class SimpleGoal : Goal
 {
-    
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) 
+        : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        if (!_isComplete)
+        {
+            _isComplete = true;
+            Console.WriteLine($"Goal '{_shortName}' completed! Earned {_points} points.");
+        }
+    }
+
+    public override bool IsComplete() => _isComplete;
+
+    public override string GetDetailsString() => _isComplete ? "[X] " + _shortName : "[ ] " + _shortName;
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal|{_shortName}|{_description}|{_points}|{_isComplete}";
+    }
 }
